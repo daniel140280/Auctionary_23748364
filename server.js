3f116fb4359597478f11b735173e55edd4afa12f
Nicky Lai <nickylai@Nickys-MacBook-Air.local>
2025-12-06 22:21:48 +0000

    Refactor server.js for consistency and readability

--- server.js
+++ server.js
@@ -1,39 +1,42 @@
-const express = require('express');
-const morgan  = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
+
+const apiRouter = express.Router();
 
 // Server port
 const HTTP_PORT = 3333;
 
 // Start server
 app.listen(HTTP_PORT, () => {
-    console.log('Server running on port: ' + HTTP_PORT);
+  console.log("Server running on port: " + HTTP_PORT);
 });
 
 // Logging
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
 // Body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Root endpoint
-app.get('/', (req, res, next) => {
-    res.json({'status': 'Alive'});
+app.get("/", (req, res, next) => {
+  res.json({ status: "Alive" });
 });
 
 // Other API endpoints: Links go here...
-require('./app/routes/user.server.routes')(app);
-require('./app/routes/core.server.routes')(app);
-require('./app/routes/question.server.routes')(app);
+require("./app/routes/user.server.routes")(apiRouter);
+require("./app/routes/core.server.routes")(apiRouter);
+require("./app/routes/question.server.routes")(apiRouter);
 
+// mount the router under /api
+app.use("/api", apiRouter);
 
 // Default response for any other request
 app.use((req, res) => {
-    res.sendStatus(404);
+  res.sendStatus(404);
 });
